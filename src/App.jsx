3a6bd692946d6602e7a6f4d05de8ced33e2daf68
Daniel Chima <dcnnad@gmail.com>
2026-01-28 20:26:51 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Navbar from './components/Navbar'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
+
 function App() {
   return (
     <div className="app">
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
